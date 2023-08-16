6a1ef78a46cab732df69a85150a1cfe1115ea5e0
AiGaA <>
2023-08-16 01:36:54 +0000

    Fix database migration issue

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
 import os
 import dj_database_url
-if os.path.isfile('env.py'):
+
+if os.path.exists('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,7 +88,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
 
 # Password validation
